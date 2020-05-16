5019d925a0f59736b8adb76f4a8a83cc4ebfaa8b
Seiichi Ariga <seiichi.ariga@gmail.com>
2020-05-17 02:11:53 +0900

    'DB作成済み'

--- data/roast-db.sql
+++ data/roast-db.sql
@@ -1,21 +1,21 @@
 CREATE TABLE roasts(
-    roast_id INTEGER NOT NULL PRIMARY KEY,
-    roast_day AS TEXT,
-    bean AS TEXT,
-    crop AS TEXT,
-    grams AS REAL,
-    roast_time AS INTEGER NOT NULL,
-    eject_time AS INTEGER DEFAULT 0,
-    roast AS INTEGER
+    roast_id integer NOT NULL PRIMARY KEY,
+    roast_day,
+    bean,
+    crop,
+    grams,
+    roast_time NOT NULL,
+    eject_time NOT NULL default 0,
+    result
 );
 
 PRAGMA foreign_keys = ON;
 
 CREATE TABLE roast_time_temp(
-    id AS INTEGER PRIMARY KEY AUTOINCREMENT,
-    roast_id INTEGER NOT NULL,
-    remain_min AS INTEGER NOT NULL,
-    remain_sec AS INTEGER NOT NULL,
-    temp AS REAL NOT NULL,
+    id INTEGER primary key autoincrement,
+    roast_id not null,
+    remain_min not null,
+    remain_sec not null,
+    temp not null,
     FOREIGN KEY (roast_id) REFERENCES roasts(roast_id)
 );
